ebbe4efa4c96ff3d73590b38d7f1afc802b6ed2b
Hatamleh <hatem.hatamleh@gmail.com>
2023-03-05 23:36:10 +0100

    📦 NEW: add server!

---
+++ backend/routes/info.js
@@ -0,0 +1,132 @@
+import express from 'express';
+
+const infoRouter = express.Router();
+
+const courses = [
+	{
+		id: 1,
+		author: 'Hatem Hatamleh',
+		name: 'Selenium',
+		type: 'WEB',
+		language: 'JAVA',
+	},
+	{
+		id: 2,
+		author: 'Hatem Hatamleh',
+		name: 'ISTQB Foundation Level',
+		type: 'MANUAL',
+		language: 'NONE',
+	},
+];
+
+const lectures = [
+	{
+		id: 1,
+		name: 'Selenium Introduction',
+		type: 'FREE',
+		mode: 'VIDEO',
+	},
+	{
+		id: 2,
+		name: 'Selenium Architecture',
+		type: 'PAID',
+		mode: 'VIDEO',
+	},
+	{
+		id: 3,
+		name: 'Install Java',
+		type: 'PAID',
+		mode: 'VIDEO',
+	},
+	{
+		id: 4,
+		name: 'WebDriver Manager',
+		type: 'PAID',
+		mode: 'ARTICLE',
+	},
+	{
+		id: 5,
+		name: 'Introduction to ISTQB',
+		type: 'PAID',
+		mode: 'VIDEO',
+	},
+	{
+		id: 6,
+		name: 'What is testing',
+		type: 'PAID',
+		mode: 'VIDEO',
+	},
+];
+
+infoRouter.get('/courses', (req, res) => {
+	const { type, language } = req.headers;
+
+	if (type && language) {
+		const flCourses = courses.filter(
+			(course) => course.type === type && course.language === language
+		);
+		return res.status(200).json({
+			courses: flCourses,
+			count: flCourses.length,
+		});
+	}
+
+	if (type) {
+		const flCourses = courses.filter((course) => course.type === type);
+		return res.status(200).json({
+			courses: flCourses,
+			count: flCourses.length,
+		});
+	}
+
+	if (language) {
+		const flCourses = courses.filter((course) => course.language === language);
+		return res.status(200).json({
+			courses: flCourses,
+			count: flCourses.length,
+		});
+	}
+
+	return res.status(200).json({
+		courses,
+		count: courses.length,
+	});
+});
+
+infoRouter.get('/lectures', (req, res) => {
+	const type = req.query.type;
+	const mode = req.query.mode;
+	console.log(type, mode);
+	if (type && mode) {
+		const flCourses = lectures.filter(
+			(lecture) => lecture.mode === mode && lecture.type === type
+		);
+		return res.status(200).json({
+			lectures: flCourses,
+			count: flCourses.length,
+		});
+	}
+
+	if (type) {
+		const flCourses = lectures.filter((lecture) => lecture.type === type);
+		return res.status(200).json({
+			lectures: flCourses,
+			count: flCourses.length,
+		});
+	}
+
+	if (mode) {
+		console.log('I am here');
+		const flCourses = lectures.filter((lecture) => lecture.mode === mode);
+		return res.status(200).json({
+			lectures: flCourses,
+			count: flCourses.length,
+		});
+	}
+	return res.status(200).json({
+		lectures,
+		count: lectures.length,
+	});
+});
+
+export default infoRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
-import express from "express";
+import express from 'express';
 const app = express();
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import mongoose from "mongoose";
-import authenticateToken from "./middlewares/auth.js";
-import cors from "cors";
-import path from "path";
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+import authenticateToken from './middlewares/auth.js';
+import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 
 //Import Routes
-import UserRoutes from "./routes/users.js";
-import TaskRoutes from "./routes/tasks.js";
-import seedRouter from "./routes/seedRoutes.js";
+import UserRoutes from './routes/users.js';
+import TaskRoutes from './routes/tasks.js';
+import seedRouter from './routes/seedRoutes.js';
+import infoRouter from './routes/info.js';
 
 const __dirname = path.resolve();
 
@@ -21,27 +22,34 @@ const port = process.env.PORT || 8080;
 
 //Connect to DB
 try {
-  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-    console.log("DataBase is up and running");
-  });
+	mongoose.connect(
+		process.env.MONGODB_URI,
+		{ useNewUrlParser: true, useUnifiedTopology: true },
+		() => {
+			console.log('DataBase is up and running');
+		}
+	);
 } catch (err) {
-  console.log("Something went wrong, cant connect to database");
+	console.log('Something went wrong, cant connect to database');
 }
 
 // MiddleWears
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 // Use Routes
-app.use("/api/v1/users", UserRoutes);
-app.use("/api/v1/tasks", authenticateToken, TaskRoutes);
-app.use("/api/v1/seed", seedRouter);
+app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1/tasks', authenticateToken, TaskRoutes);
+app.use('/api/v1/seed', seedRouter);
+app.use('/api/v1/info', infoRouter);
 
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/frontend/build/index.html")));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+	res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
 app.listen(port, () => {
-  console.log(`Server is up and running on port ${port}`);
+	console.log(`Server is up and running on port ${port}`);
 });
